eaebd4eb2adb1dd6858c00da26edce6e5e9c001c
saki000 <hangchengtengqi@gmail.com>
2022-12-17 15:00:12 +0900

    Authのファイル名を変更

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,34 @@
 import { MantineProvider } from "@mantine/core";
 import { useSelector } from "react-redux";
 import "./App.css";
-import Login from "./features/login/Login";
-import Logined from "./features/login/Logined";
+import Login from "./features/auth/Login";
+import Authed from "./features/auth/Logined";
 import { selectUser } from "./redux/userSlice";
 
 function App() {
   const user = useSelector(selectUser);
   return (
-    <MantineProvider withGlobalStyles withNormalizeCSS>
-      {user.uid ? <Logined /> : <Login />}
+    <MantineProvider
+      theme={{
+        colors: {
+          brown: [
+            "#E0E0E0",
+            "#CCCCCC",
+            "#B8B8B8",
+            "#A3A3A3",
+            "#8F8F8F",
+            "#7A7A7A",
+            "#666666",
+            "#595959",
+            "#4D4D4D",
+            "#404040",
+          ],
+        },
+      }}
+      withGlobalStyles
+      withNormalizeCSS
+    >
+      {user.uid ? <Authed /> : <Login />}
     </MantineProvider>
   );
 }
---
+++ src/features/auth/Logined.tsx
@@ -0,0 +1,28 @@
+import { AppShell, Header } from "@mantine/core";
+import { RecoilRoot } from "recoil";
+import Head from "../../components/head/Header";
+import Main from "../../components/Main";
+
+const Authed = () => {
+  return (
+    <RecoilRoot>
+      <AppShell
+        header={
+          <Header height={70} className="bg-darkBlue">
+            <Head></Head>
+          </Header>
+        }
+        className="bg-darkBlue h-screen"
+        styles={() => ({
+          body: { height: "100%" },
+        })}
+      >
+        <div className="h-full">
+          <Main />
+        </div>
+      </AppShell>
+    </RecoilRoot>
+  );
+};
+
+export default Authed;
