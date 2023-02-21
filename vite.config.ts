c9b8782c228453210033e5b1b58ba97cf4f40e22
saki000 <hangchengtengqi@gmail.com>
2023-02-21 16:06:21 +0900

    craからviteへ以降

---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+  plugins: [react(), tsconfigPaths()],
+  build: {
+    outDir: "build", // CRAに合わせて指定
+  },
+});
