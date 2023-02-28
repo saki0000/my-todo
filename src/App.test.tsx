9e842c3a217d0561ab545a4d1e1148e19c95e582
saki000 <hangchengtengqi@gmail.com>
2023-02-28 17:39:24 +0900

    vitestの導入

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,15 +1,13 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
+import { render, screen } from "@testing-library/react";
 
-test('renders learn react link', () => {
-  const { getByText } = render(
-    <Provider store={store}>
-      <App />
-    </Provider>
-  );
+import App from "./App";
 
-  expect(getByText(/learn/i)).toBeInTheDocument();
+describe("App", () => {
+  it("renders headline", () => {
+    render(<App />);
+
+    screen.debug();
+
+    // check if App components renders headline
+  });
 });
---
+++ vitest.config.ts
@@ -0,0 +1,9 @@
+// vite.config.ts
+import { defineConfig } from "vitest/config";
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.ts",
+  },
+});
